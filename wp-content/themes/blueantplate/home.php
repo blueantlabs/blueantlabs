581ea4c048e27ae3d2cc668bd1f4d608d0d1c64a
Alex Vance <alex@blueantlabs.com>
2013-05-07 00:18:46 -0400

    weird transparent cool state

--- wp-content/themes/blueantplate/home.php
+++ wp-content/themes/blueantplate/home.php
@@ -1,24 +1,26 @@
 <?php get_header(); ?>
 
-	<div id="home-content" class="home-img">
-		<a id="port-tfp" href="<?php bloginfo('url') ?>/portfolio" alt="The Future Project" >
-      <img src="<?php bloginfo('url') ?>/img/port-wide/thefutureproject-605.png" />
-    </a>
-		<a id="port-icurt" href="<?php bloginfo('url') ?>/portfolio" alt="iPhone Curt" >
-      <img src="<?php bloginfo('url') ?>/img/port-wide/iphonecurt-605.png" />
-    </a>
-		<a id="port-vside" href="<?php bloginfo('url') ?>/portfolio" alt="Villeside Promotions" >
-      <img src="<?php bloginfo('url') ?>/img/port-wide/vside-605.png" />
-    </a>
-		<a id="port-rlw" href="<?php bloginfo('url') ?>/portfolio" alt="Read Learn Write" >
-      <img src="<?php bloginfo('url') ?>/img/port-wide/readlearnwrite-605.png" />
-    </a>
-		<a id="port-theights" href="<?php bloginfo('url') ?>/portfolio" alt="Thetis Heights" >
-      <img src="<?php bloginfo('url') ?>/img/port-wide/theights-605.png" />
-    </a>
-		<a id="port-mobi" href="<?php bloginfo('url') ?>/portfolio" alt="mobiLIFE, Inc." >
-      <img src="<?php bloginfo('url') ?>/img/port-wide/mobi-605.png" />
-    </a>
-	</div>
+<div id="home-content" class="home-img">
+  <ul>
+  	<li><a id="port-tfp" href="<?php bloginfo('url') ?>/portfolio" alt="The Future Project" >
+      <img src="<?php bloginfo('url') ?>/wp-content/themes/blueantplate/img/port-wide/thefutureproject-605.png" />
+    </a></li>
+  	<li><a id="port-icurt" href="<?php bloginfo('url') ?>/portfolio" alt="iPhone Curt" >
+      <img src="<?php bloginfo('url') ?>/wp-content/themes/blueantplate/img/port-wide/iphonecurt-605.png" />
+    </a></li>
+  	<li><a id="port-vside" href="<?php bloginfo('url') ?>/portfolio" alt="Villeside Promotions" >
+      <img src="<?php bloginfo('url') ?>/wp-content/themes/blueantplate/img/port-wide/vside-605.png" />
+    </a></li>
+  	<li><a id="port-rlw" href="<?php bloginfo('url') ?>/portfolio" alt="Read Learn Write" >
+      <img src="<?php bloginfo('url') ?>/wp-content/themes/blueantplate/img/port-wide/readlearnwrite-605.png" />
+    </a></li>
+  	<li><a id="port-theights" href="<?php bloginfo('url') ?>/portfolio" alt="Thetis Heights" >
+      <img src="<?php bloginfo('url') ?>/wp-content/themes/blueantplate/img/port-wide/theights-605.png" />
+    </a></li>
+  	<li><a id="port-mobi" href="<?php bloginfo('url') ?>/portfolio" alt="mobiLIFE, Inc." >
+      <img src="<?php bloginfo('url') ?>/wp-content/themes/blueantplate/img/port-wide/mobi-605.png" />
+    </a></li>
+  </ul>
+</div>
 
 <?php get_footer(); ?>
